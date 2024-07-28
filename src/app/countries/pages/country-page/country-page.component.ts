8414c2ee4f356bcf11dba786d26adfd05865b1cc
Sebastian <Sebastianballesterosvargas@outlook.com>
2024-07-28 11:31:39 -0500

    Se adiciona código base del proyecto

---
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'contries-country-page',
+  templateUrl: './country-page.component.html',
+  styles: ``
+})
+export class CountryPageComponent {
+
+}
